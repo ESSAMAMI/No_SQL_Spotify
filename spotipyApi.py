62f9a5ee08e5cf8d7ffb09504df238e78d73d4da
Dreamsplutox <arnaudsimon091@gmail.com>
2019-07-03 17:53:08 +0200

    python code to export SpotifyApi data in a JSON file

--- spotipyApi.py
+++ spotipyApi.py
@@ -1,34 +1,54 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import json
+import fileinput
+import re
+ 
+from pprint import pprint
+from jsonmerge import Merger
 
-
-cid ="6ac501309e3b4f5c83c9cf0af07ec7d6"
-secret = "8e23f5a287ea43b5af5a3d58634299cb"
+cid ="b7e280e5107441f586f666e17be0861a"
+secret = "3798e902795248c8b4352ffcd4fec6f7"
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 track_id = track_name = popularity = artist_name = []
 
-for i in range(0,10000, 50):
-    print('=============== Generate data  iterat n° {} / 10000 ==============='.format(i+1))
+#premier échantillion de données récupéré qui va nous donner la structure JSON
+track_results_A = sp.search(q='year:2018', type='track', limit=50,offset=0)
+
+#var avec tous les items, ici on récup les x premiers items et à chaque itération 
+#on va merge, on aura aiinsi un json complet à la fin
+all_items = track_results_A['tracks']['items']
+
+#parcours des données et récupération à chaque fois des items
+for i in range(50,10000, 50):
+    print('=============== Generate data  iterat num %s / 10000 ==============='%(i+1))
     track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
+    all_items.append(track_results['tracks']['items'])
+    
+
+
 
     for j, t in enumerate(track_results['tracks']['items']):
-        # print('=============== Epochs n° {} / {} / 10000 ==============='.format((j+1),(i+1)))
         artist_name.append(t['artists'][0]['name'])
         track_name.append(t['name'])
         track_id.append(t['id'])
         popularity.append(t['popularity'])
 
-    break
-        # print('{} - {} - {} - {}'.format(t['artists'][0]['name'], t['name'], t['id'], t['popularity']))
+    #break
+        #print('{} - {} - {} - {}'.format(t['artists'][0]['name'], t['name'], t['id'], t['popularity']))
+
+#ecrire flux dans fichier
+with open('output_data.json', 'w') as f:
+    json.dump(track_results_A, f)
+
 
 print('\nLen of artist_name = {} \nLen of track_name = {} \nLen of popularity = {} \nLen of track_id = {} '.format(len(artist_name), len(track_name), len(popularity), len(track_id)))
 
-json_track_id = json.loads(track_id)
-print(json_track_id)
+#json_track_id = json.loads(track_id)
+#print(json_track_id)
 
 # for idx in range(len(track_id)):
 #
