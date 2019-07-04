48032663441621e2091282bd8ea2285963d94805
ESSAMAMI <hamza.essamami.sio@gmail.com>
2019-07-04 23:17:33 +0200

    spotify to mongodb Ok

--- spotipyApi.py
+++ spotipyApi.py
@@ -3,9 +3,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import json
 import fileinput
 import re
- 
 from pprint import pprint
-from jsonmerge import Merger
+# from jsonmerge import Merger
 
 cid ="b7e280e5107441f586f666e17be0861a"
 secret = "3798e902795248c8b4352ffcd4fec6f7"
@@ -18,7 +17,7 @@ track_id = track_name = popularity = artist_name = []
 #premier échantillion de données récupéré qui va nous donner la structure JSON
 track_results_A = sp.search(q='year:2018', type='track', limit=50,offset=0)
 
-#var avec tous les items, ici on récup les x premiers items et à chaque itération 
+#var avec tous les items, ici on récup les x premiers items et à chaque itération
 #on va merge, on aura aiinsi un json complet à la fin
 all_items = track_results_A['tracks']['items']
 
@@ -27,7 +26,7 @@ for i in range(50,10000, 50):
     print('=============== Generate data  iterat num %s / 10000 ==============='%(i+1))
     track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
     all_items.append(track_results['tracks']['items'])
-    
+
 
 
 
@@ -36,13 +35,13 @@ for i in range(50,10000, 50):
         track_name.append(t['name'])
         track_id.append(t['id'])
         popularity.append(t['popularity'])
-
+        print(len(popularity))
     #break
         #print('{} - {} - {} - {}'.format(t['artists'][0]['name'], t['name'], t['id'], t['popularity']))
 
 #ecrire flux dans fichier
-with open('output_data.json', 'w') as f:
-    json.dump(track_results_A, f)
+# with open('output_data.json', 'w') as f:
+#     json.dump(track_results_A, f)
 
 
 print('\nLen of artist_name = {} \nLen of track_name = {} \nLen of popularity = {} \nLen of track_id = {} '.format(len(artist_name), len(track_name), len(popularity), len(track_id)))
